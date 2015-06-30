92d1fe56b62e4a9c68626fe2425ad9c3c8b99ed6
cilnoalsea <cilnoalsea@gmail.com>
2015-06-30 11:57:53 +0200

    v2

--- pi.c
+++ pi.c
@@ -14,11 +14,14 @@ MPI_Comm_size(MPI_COMM_WORLD,&numprocs);
 MPI_Comm_rank(MPI_COMM_WORLD,&myid);
 if (myid == 0) {
 fflush(stdout); scanf("%d",&n);
+time = MPI_Wtime();
 }
 MPI_Bcast(&n, 1, MPI_INT, 0, MPI_COMM_WORLD);
 h = 1.0 / (double) n;
 sum = 0.0;
 for (i = myid + 1; i <= n; i += numprocs) {
+	x = h * ((double)i - 0.5);
+	sum += 4.0 / (1.0 + x*x);
 }
 mypi = h * sum;
 MPI_Reduce(&mypi, &pi, 1, MPI_DOUBLE, MPI_SUM, 0,
