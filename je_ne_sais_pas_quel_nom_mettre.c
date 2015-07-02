5e3961a31426af0ba887b003c32b6fece66c1435
cilnoalsea <cilnoalsea@gmail.com>
2015-07-02 12:14:20 +0200

    je n'aurais jamais du mettre un nom pareil

---
+++ je_ne_sais_pas_quel_nom_mettre.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <omp.h>
+#include <math.h>
+#include <time.h>   
+#include "mpi.h"
+
+int main (int argc, char **argv)
+	{
+		int i, j, myid, numprocs, pinkie_pie, apple_jack;
+		MPI_Init(&argc,&argv);
+		MPI_Comm_size(MPI_COMM_WORLD,&numprocs);
+		MPI_Comm_rank(MPI_COMM_WORLD,&myid);
+		for(i=0; i<numprocs; i++)
+			{
+				j=(i+1)%numprocs;
+				MPI_Sendrecv(&pinkie_pie, 1, MPI_INT, i, 42, &apple_jack, 1, 
+				        MPI_INT, j, 42, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+			}
+	}
